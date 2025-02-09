2cd0ffa05d49328289b8bc5c53bad0cf1ffb751f
Ketan Kauntia <ketankauntia26@gmail.com>
2025-02-09 11:58:21 +0530

    signup done via signUp webhook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,11 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 
 export default function Home() {
-  return <div className='m-10'>homepage</div>;
+  return (
+    <div className='m-10'>
+      <SignInButton>
+        <Button>Sign In</Button>
+      </SignInButton>
+    </div>
+  );
 }
